6b505c9149b0e781d0c1e72a656b149e1e16bb41
Helenn Melo <hnmelo9@misena.edu.co>
2021-09-22 21:10:22 -0500

    identación1

--- CLASS/conexion.php
+++ CLASS/conexion.php
@@ -1,21 +1,21 @@
 <?php
     class conexion{
-       private $conect;
+        private $conect;
 
-       public function_construct(){
-           $connectionString = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";.DB_CHARSET.";
-           try {
-               $this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
-               $this->conect = setAttribute(PDO::ATT_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            public function_construct(){
+            $connectionString = "mysql:host=".DB_HOST.";dbname=".DB_NAME.";.DB_CHARSET.";
+            try {
+                $this->conect = new PDO($connectionString, DB_USER, DB_PASSWORD);
+                $this->conect = setAttribute(PDO::ATT_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-           } catch (PDOException $e) {
-               $this->conect = 'Error de conexión';
-               echo "ERROR: ".$e->getMessage();
-           }
-       }
+            } catch (PDOException $e) {
+                $this->conect = 'Error de conexión';
+                echo "ERROR: ".$e->getMessage();
+            }
+        }
 
-       public function conect(){
-           return $this->conect;
-       }
+        public function conect(){
+        return $this->conect;
+        }
     }
 ?>
